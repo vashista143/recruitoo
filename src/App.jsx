c30830efb330448720c941f54180de101d4827fd
vashista <82438452+vashista143@users.noreply.github.com>
2025-12-28 09:58:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -102,9 +102,13 @@ useEffect(() => {
 
   if (!authChecked) {
     return (
-      <div className="flex justify-center items-center h-screen text-lg font-medium">
-        Checking session...
-      </div>
+      <div className="flex justify-center items-center h-screen">
+  <div className="flex gap-2">
+    <span className="w-3 h-3 bg-blue-500 rounded-full animate-bounce"></span>
+    <span className="w-3 h-3 bg-blue-500 rounded-full animate-bounce [animation-delay:0.15s]"></span>
+    <span className="w-3 h-3 bg-blue-500 rounded-full animate-bounce [animation-delay:0.3s]"></span>
+  </div>
+</div>
     );
   }
 console.log(appliedjobs)
